7ad4d65f4c1b946875095c5e63fff08d715f4022
ihebmeftah <ihebmeftah666@gmail.com>
2025-06-20 21:19:44 +0100

    dd static file serving

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -11,7 +13,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
   // Use default port 3000 if process.env.PORT is not set or not a number
   const port = parseInt(process.env.PORT || '3000', 10);
   await app.listen(port);

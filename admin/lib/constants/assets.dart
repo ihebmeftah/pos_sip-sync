9bf5d8256603287a4b541fc24b4d3e45300f8b5d
ihebmeftah <ihebmeftah666@gmail.com>
2025-08-29 18:29:35 +0100

    fix: Remove unused dashboard image and format asset strings for consistency

--- admin/lib/constants/assets.dart
+++ admin/lib/constants/assets.dart
@@ -1,14 +1,16 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-  
+
   /// Assets for assetsImagesSvgDashboard
   /// assets/images/svg/dashboard.svg
-  static const String assetsImagesSvgDashboard = "assets/images/svg/dashboard.svg";
+  static const String assetsImagesSvgDashboard =
+      "assets/images/svg/dashboard.svg";
 
   /// Assets for assetsImagesSvgInventory
   /// assets/images/svg/inventory.svg
-  static const String assetsImagesSvgInventory = "assets/images/svg/inventory.svg";
+  static const String assetsImagesSvgInventory =
+      "assets/images/svg/inventory.svg";
 
   /// Assets for assetsImagesSvgOrder
   /// assets/images/svg/order.svg
@@ -18,4 +20,3 @@ class Assets {
   /// assets/images/svg/table.svg
   static const String assetsImagesSvgTable = "assets/images/svg/table.svg";
 }
-
